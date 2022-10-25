dc412952ce1c424e87f6bf69976613a261ee8d6b
Valentin <choval@orange.fr>
2022-10-25 11:32:25 +0200

    platforme

--- platformeur/Form1.Designer.cs
+++ platformeur/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.moove = new System.Windows.Forms.Timer(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.monstre = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
@@ -44,12 +44,13 @@
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
-            this.compteurpiece = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monstre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
@@ -58,16 +59,16 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
             this.SuspendLayout();
             // 
             // player
             // 
             this.player.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
-            this.player.Location = new System.Drawing.Point(47, 137);
-            this.player.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.player.Location = new System.Drawing.Point(34, 571);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(54, 65);
+            this.player.Size = new System.Drawing.Size(62, 87);
             this.player.TabIndex = 0;
             this.player.TabStop = false;
             this.player.Click += new System.EventHandler(this.player_Click);
@@ -75,10 +76,9 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Lime;
-            this.pictureBox1.Location = new System.Drawing.Point(47, 303);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 655);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(247, 26);
+            this.pictureBox1.Size = new System.Drawing.Size(282, 35);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Tag = "support";
@@ -93,10 +93,9 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Lime;
-            this.pictureBox2.Location = new System.Drawing.Point(359, 190);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(416, 571);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(144, 26);
+            this.pictureBox2.Size = new System.Drawing.Size(165, 35);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Tag = "support";
@@ -105,32 +104,32 @@
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Lime;
-            this.pictureBox3.Location = new System.Drawing.Point(150, 80);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(171, 349);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(144, 26);
+            this.pictureBox3.Size = new System.Drawing.Size(225, 35);
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Tag = "support";
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
-            // pictureBox4
+            // monstre
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.DarkOrchid;
-            this.pictureBox4.Location = new System.Drawing.Point(172, 31);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(32, 32);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Tag = "monstre";
+            this.monstre.BackColor = System.Drawing.Color.DarkOrchid;
+            this.monstre.Location = new System.Drawing.Point(225, 160);
+            this.monstre.Name = "monstre";
+            this.monstre.Size = new System.Drawing.Size(37, 43);
+            this.monstre.TabIndex = 4;
+            this.monstre.TabStop = false;
+            this.monstre.Tag = "monstre";
+            this.monstre.Click += new System.EventHandler(this.monstre_Click);
             // 
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox5.Location = new System.Drawing.Point(112, 266);
+            this.pictureBox5.Location = new System.Drawing.Point(171, 605);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox5.Size = new System.Drawing.Size(31, 43);
             this.pictureBox5.TabIndex = 5;
             this.pictureBox5.TabStop = false;
             this.pictureBox5.Tag = "piece";
@@ -138,9 +137,10 @@
             // pictureBox6
             // 
             this.pictureBox6.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox6.Location = new System.Drawing.Point(249, 266);
+            this.pictureBox6.Location = new System.Drawing.Point(285, 605);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox6.Size = new System.Drawing.Size(31, 43);
             this.pictureBox6.TabIndex = 6;
             this.pictureBox6.TabStop = false;
             this.pictureBox6.Tag = "piece";
@@ -148,19 +148,22 @@
             // pictureBox7
             // 
             this.pictureBox7.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox7.Location = new System.Drawing.Point(216, 266);
+            this.pictureBox7.Location = new System.Drawing.Point(285, 299);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox7.Size = new System.Drawing.Size(31, 43);
             this.pictureBox7.TabIndex = 7;
             this.pictureBox7.TabStop = false;
             this.pictureBox7.Tag = "piece";
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
             // 
             // pictureBox8
             // 
             this.pictureBox8.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox8.Location = new System.Drawing.Point(183, 266);
+            this.pictureBox8.Location = new System.Drawing.Point(231, 605);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox8.Size = new System.Drawing.Size(31, 43);
             this.pictureBox8.TabIndex = 8;
             this.pictureBox8.TabStop = false;
             this.pictureBox8.Tag = "piece";
@@ -168,9 +171,10 @@
             // pictureBox9
             // 
             this.pictureBox9.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox9.Location = new System.Drawing.Point(150, 266);
+            this.pictureBox9.Location = new System.Drawing.Point(231, 299);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox9.Size = new System.Drawing.Size(31, 43);
             this.pictureBox9.TabIndex = 9;
             this.pictureBox9.TabStop = false;
             this.pictureBox9.Tag = "piece";
@@ -178,20 +182,21 @@
             // pictureBox10
             // 
             this.pictureBox10.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox10.Location = new System.Drawing.Point(388, 153);
+            this.pictureBox10.Location = new System.Drawing.Point(429, 521);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox10.Size = new System.Drawing.Size(31, 43);
             this.pictureBox10.TabIndex = 10;
             this.pictureBox10.TabStop = false;
             this.pictureBox10.Tag = "piece";
-        
             // 
             // pictureBox11
             // 
             this.pictureBox11.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox11.Location = new System.Drawing.Point(421, 153);
+            this.pictureBox11.Location = new System.Drawing.Point(488, 521);
+            this.pictureBox11.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox11.Size = new System.Drawing.Size(31, 43);
             this.pictureBox11.TabIndex = 11;
             this.pictureBox11.TabStop = false;
             this.pictureBox11.Tag = "piece";
@@ -199,26 +204,48 @@
             // pictureBox12
             // 
             this.pictureBox12.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox12.Location = new System.Drawing.Point(454, 153);
+            this.pictureBox12.Location = new System.Drawing.Point(549, 521);
+            this.pictureBox12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(27, 32);
+            this.pictureBox12.Size = new System.Drawing.Size(31, 43);
             this.pictureBox12.TabIndex = 12;
             this.pictureBox12.TabStop = false;
             this.pictureBox12.Tag = "piece";
             // 
-            // compteurpiece
+            // label1
             // 
-            this.compteurpiece.Enabled = true;
-            this.compteurpiece.Tag = "compteurp";
-            this.compteurpiece.Tick += new System.EventHandler(this.compteurpiece_Tick);
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(703, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Tag = "compteurpiece";
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.label1.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.pieceAdd);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox13.Location = new System.Drawing.Point(633, 492);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(165, 35);
+            this.pictureBox13.TabIndex = 14;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Tag = "supportmoove";
+            this.pictureBox13.Click += new System.EventHandler(this.pictureBox13_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(1421, 787);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox12);
             this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.pictureBox10);
@@ -227,12 +254,11 @@
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.monstre);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.player);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -242,7 +268,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monstre)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
@@ -251,7 +277,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -262,8 +290,7 @@
         private System.Windows.Forms.Timer moove;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
-        private HelpProvider helpProvider1;
+        private PictureBox monstre;
         private PictureBox pictureBox5;
         private PictureBox pictureBox6;
         private PictureBox pictureBox7;
@@ -272,6 +299,7 @@
         private PictureBox pictureBox10;
         private PictureBox pictureBox11;
         private PictureBox pictureBox12;
-        private System.Windows.Forms.Timer compteurpiece;
+        private Label label1;
+        private PictureBox pictureBox13;
     }
 }
--- platformeur/Form1.cs
+++ platformeur/Form1.cs
@@ -7,6 +7,7 @@ namespace platformeur
         int saut =30;
         int timesaut = 0;
         int vitesse=10;
+        int vitessem = 3;
         int gravitation = 15 ;
         int piece = 0;
         public Form1()
@@ -33,16 +34,61 @@ namespace platformeur
         {
 
         }
+        private void gravity()
+        {
+            foreach (Control m in this.Controls)
+            {
+                if (m is PictureBox && (string)m.Tag == "monstre")
+                {
+                    m.Top += gravitation;
+                    m.BringToFront();
+                    m.Left += vitessem;
+                }
+            }
+        }
+
+        private void plateform()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "supportmoove")
+                {
+
+                    x.Left += vitessem;
+                    if (x.Left < 633 ||x.Left > 1100)
+                    {
+                        vitessem = -vitessem;
+                    }
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        player.Top -= gravitation;
+                        timesaut = 10;
+                        if (x.Left < 633 || x.Left > 1100)
+                        {
+                            player.Left -= vitessem;
+                        }
+                        else
+                        {
+                            player.Left += vitessem;
+                        }
+                    }
+                }
+            }
+        }
 
         private void mooveevent(object sender, EventArgs e)
         {
             saut = 30;
             player.Top += gravitation;
+            gravity();
+            plateform();
+           
+          
             if (gauche == true && player.Left > 0)
             {
                 player.Left -= vitesse;
             }
-            if (droite == true && player.Left < 750)
+            if (droite == true && player.Left < 2000)
             {
                 player.Left += vitesse;
             }
@@ -53,38 +99,53 @@ namespace platformeur
                 saut -= 2;
             }
             foreach (Control x in this.Controls)
-                foreach (Control y in this.Controls)
+            {
+                foreach (Control m in this.Controls)
                 {
-
+                    if (m is PictureBox && (string)m.Tag == "monstre")
                     {
 
-                        if (x is PictureBox && (string)x.Tag == "support")
-
+                        if (player.Bounds.IntersectsWith(m.Bounds))
                         {
-                            if (y is PictureBox && (string)y.Tag == "monstre")
-                            {
-                                y.Top += gravitation;
-                                if (y.Bounds.IntersectsWith(x.Bounds))
-                                {
-                                    y.Top -= 15;
-                                }
-                            }
-                            if (player.Bounds.IntersectsWith(x.Bounds))
-                            {
-                                player.Top -= 15;
-                                timesaut = 10;
+                            this.Controls.Remove(player);
 
-
-                            }
                         }
-                        if (x is PictureBox && (string)x.Tag == "piece")
-
+                        if (x is PictureBox && (string)x.Tag == "support")
                         {
-                            
-
+                            if (m.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                m.Top -= gravitation;
+                            }
                         }
                     }
+
+                    if (x is PictureBox && (string)x.Tag == "support")
+                    {
+
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            player.Top -= gravitation;
+                            timesaut = 10;
+                        }
+                    }
+
+                    if (x is PictureBox && (string)x.Tag == "supportmoove")
+                    {
+                        
+                    }
+                    if (x is PictureBox && (string)x.Tag == "piece")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            this.Controls.Remove(x);
+                            piece += 1;
+                            this.label1.Text = piece.ToString();
+                            break;
+                        }
+
+                    }
                 }
+            }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -115,9 +176,30 @@ namespace platformeur
         }
 
 
-        private void compteurpiece_Tick(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        
+        }
+
+        private void pieceAdd(object sender, ControlEventArgs e)
         {
 
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void monstre_Click(object sender, EventArgs e)
+        {
+           
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
